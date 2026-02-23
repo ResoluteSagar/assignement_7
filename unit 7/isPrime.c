2f38ee7b2196cd43a88419e206ec859211705aaf
Sagar <karki.sagar061@gmail.com>
2026-02-23 20:27:00 +0545

    Update isPrime.c

--- unit 7/isPrime.c
+++ unit 7/isPrime.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int isPrime(int n) {
-    if (n <= 1)
-        return 0;
+int isPrime(int n){
+if(n<=1)
+return 0;
 
-    for (int i = 2; i <= n / 2; i++) {
-        if (n % i == 0)
-            return 0;
+for(int i=2;i<n;i++){
+    if(n%i==0)
+    return 0;
+    else{
+        return 1;
     }
-    return 1;
 }
 
-int main() {
-    int num = 7;
+
+}
+int main(){
+    int num;
+    printf("Enter a number:");
+    scanf("%d", &num);
 
     if (isPrime(num))
-        printf("%d is Prime\n", num);
+        printf("%d\n", 1);
     else
-        printf("%d is Not Prime\n", num);
+        printf("%d\n", 0);
 
     return 0;
 }
