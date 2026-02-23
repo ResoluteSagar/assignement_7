cd204b21f2bdf98c83ce49d074be1a510eb43d1c
Sagar <karki.sagar061@gmail.com>
2026-02-23 20:36:14 +0545

    Update fibonacci.c

--- unit 7/fibonacci.c
+++ unit 7/fibonacci.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 int fibonacci(int n) {
-    if (n==0)
-        return 0;
-    if (n==1)
-        return 1;
-
-    return fibonacci(n-1)+fibonacci(n-2);
+    if (n == 0) 
+    return 0;  
+    if (n == 1) 
+    return 1; 
+    else
+    return fibonacci(n - 1) + fibonacci(n - 2); 
 }
-int main(){
-    int n = 6;
-    printf("Fibonacci of %d =%d\n",n,fibonacci(n));
+
+int main() {
+    int n;
+
+    printf("Enter position of the number:");
+    scanf("%d", &n);
+
+    int result = fibonacci(n); 
+    printf("Fibonacci number at position %d is %d\n", n, result);
+
     return 0;
 }
